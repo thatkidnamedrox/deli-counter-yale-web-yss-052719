e8bb4d7716d158b60673db203961828dc343c412
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 04:24:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,15 +5,14 @@ katz_deli = []
 def line(deli)
   if (deli.size() < 1)
     puts "The line is currently empty."
-    return
-  end
+  else
+    message = "The line is currently:"
+    numbered_list = ""
+    deli.each_with_index do |name, index|
+      numbered_list+= " #{index+1}. #{name}"
+    end
 
-  message = "The line is currently:"
-  numbered_list = ""
-  deli.each_with_index do |name, index|
-    numbered_list+= " #{index+1}. #{name}"
+    message.concat(numbered_list)
+    puts message
   end
-
-  message.concat(numbered_list)
-  puts message
 end
