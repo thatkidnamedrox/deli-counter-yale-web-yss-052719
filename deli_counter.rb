1d7e8820bc5e154781910fbd45babe7d84184fe1
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 04:31:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 katz_deli = []
 
 def line(deli)
-  if (deli.size() < 1)
+  if deli.size() < 1
     puts "The line is currently empty."
   else
     message = "The line is currently:"
@@ -14,5 +14,19 @@ def line(deli)
 
     message.concat(numbered_list)
     puts message
+  end
+end
+
+def take_a_number(line, name)
+  position = line.size() + 1
+  puts "Welcome, #{name}. You are number #{position} in line."
+end
+
+def now_serving(deli)
+  if deli.size() < 1
+    puts "There is nobody waiting to be served!"
+  else
+    name = deli.shift()
+    puts "Currently seving #{name}."
   end
 end
