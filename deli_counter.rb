69078f1fae002edab2fa04fccd3ae1e0fe9116b8
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 04:37:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(deli)
+  if deli.size() < 1
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+    numbered_list = ""
+    deli.each_with_index do |name, index|
+      numbered_list+= " #{index+1}. #{name}"
+    end
+
+    message.concat(numbered_list)
+    puts message
+  end
+end
+
+def take_a_number(line, name)
+  line.push(name)
+
+  position = line.size()
+  puts "Welcome, #{name}. You are number #{position} in line."
+end
+
+def now_serving(deli)
+  if deli.size() < 1
+    puts "There is nobody waiting to be served!"
+  else
+    name = deli.shift
+    puts "Currently serving #{name}."
+  end
+end
